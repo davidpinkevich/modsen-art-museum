dbac27f942492de5e0b4832bdaab528c17a13503
davidpinkevich <davidpinkevich95@gmail.com>
2024-04-28 15:13:58 +0300

    feat: start main layout

---
+++ src/pages/MainLayout/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header } from "@components/Header";
+
+const MainLayout: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      <p>Let's find some art here!</p>
+    </div>
+  );
+};
+
+export { MainLayout };
