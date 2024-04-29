d649779d7b6da8c7adc193dab4a5e8e8b13b4f0d
davidpinkevich <davidpinkevich95@gmail.com>
2024-04-29 22:50:37 +0300

    feat: add hook for LS

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,31 @@
+import { TypeArt } from "@src/types";
+import { useEffect, useState } from "react";
+
+const useLocalStorage = (init: TypeArt[], key: string) => {
+  const getValue = () => {
+    const store = localStorage.getItem(key);
+    if (store) {
+      return JSON.parse(store);
+    } else {
+      return init;
+    }
+  };
+  const [favorites, setSavorites] = useState(getValue);
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(favorites));
+  }, [favorites]);
+
+  const addFavorite = (item: TypeArt) => {
+    const length = favorites.filter((x: TypeArt) => x.id === item.id).length;
+    if (!length) {
+      setSavorites([...favorites, item]);
+    } else {
+      const newArr = favorites.filter((x: TypeArt) => x.id !== item.id);
+      setSavorites([...newArr]);
+    }
+  };
+
+  return [favorites, addFavorite];
+};
+
+export { useLocalStorage };
