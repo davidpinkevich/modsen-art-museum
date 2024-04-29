2a6c4c597911699492c8894569578af0dc372c4b
davidpinkevich <davidpinkevich95@gmail.com>
2024-04-29 20:36:35 +0300

    fix: change path for utils

--- src/utils/helpers/validate.ts
+++ src/utils/helpers/validate.ts
@@ -3,7 +3,7 @@ import * as yup from "yup";
 const validateSchema = yup.object().shape({
   searh: yup
     .string()
-    .matches(/^[a-zA-Z ]+$/, "Use only Latin characters for input")
+    .matches(/^[a-zA-Z]+$/, "Use only Latin characters for input")
     .max(20, "No more than 20 characters")
 });
 
