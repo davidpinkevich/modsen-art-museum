ab313ff6648ea360828a0f4548b5f96ae6dda1b3
davidpinkevich <davidpinkevich95@gmail.com>
2024-04-29 19:30:35 +0300

    feat: add loading

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,14 @@
+import { StyledLoading, SpanOne, SpanTwo, SpanThree } from "./styled";
+
+const Loading = () => {
+  return (
+    <StyledLoading>
+      Search arts
+      <SpanOne></SpanOne>
+      <SpanTwo></SpanTwo>
+      <SpanThree></SpanThree>
+    </StyledLoading>
+  );
+};
+
+export { Loading };
