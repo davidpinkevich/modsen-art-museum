beb422661aafce63ec9b831b512e26e82dcb56ba
davidpinkevich <davidpinkevich95@gmail.com>
2024-05-01 00:45:33 +0300

    fix: update sizes

--- src/utils/helpers/createImages.ts
+++ src/utils/helpers/createImages.ts
@@ -3,7 +3,7 @@ import { type TypeArt } from "@src/types";
 const createImages = (items: TypeArt[]) => {
   return items.map((item) => {
     const url = item.image_id
-      ? `${process.env.BASE_URL_IMAGES}${item.image_id}/full/200,/0/default.jpg`
+      ? `${process.env.BASE_URL_IMAGES}${item.image_id}/full/400,/0/default.jpg`
       : null;
     return {
       ...item,
