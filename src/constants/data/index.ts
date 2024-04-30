43c3bb1a1909b2626190b91d7538ea63106d9ce1
davidpinkevich <davidpinkevich95@gmail.com>
2024-04-30 22:20:54 +0300

    feat: add constants for filter

--- src/constants/data/index.ts
+++ src/constants/data/index.ts
@@ -2,4 +2,6 @@ const RANDOM_ARTS_VIEW = 9;
 
 const LENGTH_TITLE = 16;
 
-export { RANDOM_ARTS_VIEW, LENGTH_TITLE };
+const FILTERS = ["public", "not-public", "view", "not-view"];
+
+export { RANDOM_ARTS_VIEW, LENGTH_TITLE, FILTERS };
