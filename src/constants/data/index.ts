2021bfdf25cc3aca0cc9cf0a2a763e750e263d50
davidpinkevich <davidpinkevich95@gmail.com>
2024-05-01 00:46:24 +0300

    feat: add constants for arts block

--- src/constants/data/index.ts
+++ src/constants/data/index.ts
@@ -1,7 +1,24 @@
+import { TypeArtsBlock } from "@src/types";
+
 const RANDOM_ARTS_VIEW = 9;
 
 const LENGTH_TITLE = 16;
 
 const FILTERS = ["public", "not-public", "view", "not-view"];
 
-export { RANDOM_ARTS_VIEW, LENGTH_TITLE, FILTERS };
+const ARTS_BLOCK: TypeArtsBlock = {
+  main: {
+    title: "Topics for you",
+    info: "Our special gallery"
+  },
+  random: {
+    title: "Here some more",
+    info: "Other works for you"
+  },
+  favorites: {
+    title: "Saved by you",
+    info: "Your favorites list"
+  }
+};
+
+export { RANDOM_ARTS_VIEW, LENGTH_TITLE, FILTERS, ARTS_BLOCK };
