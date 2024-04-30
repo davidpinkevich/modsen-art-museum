556c99ea6e59fa740e9087d92864d2ce0fdd713f
davidpinkevich <davidpinkevich95@gmail.com>
2024-04-30 22:22:21 +0300

    feat: add styles for input

---
+++ src/components/Forms/InputSearch/styled.ts
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+const StyledInputBody = styled.div`
+  width: 100%;
+  position: relative;
+`;
+
+const StyledInput = styled.input`
+  padding: 24px 16px;
+  border-radius: 16px;
+  color: black;
+  font-size: 20px;
+  width: 100%;
+  background-color: rgba(57, 57, 57, 0.05);
+  &::placeholder {
+    font-size: 16px;
+    color: rgba(57, 57, 57, 0.5);
+  }
+  &:disabled {
+    opacity: 0.6;
+  }
+`;
+
+const StyledInputImg = styled.img`
+  position: absolute;
+  right: 16px;
+  top: 22px;
+  width: 32px;
+  height: 32px;
+`;
+
+const StyledValidate = styled.div`
+  position: absolute;
+  bottom: -24px;
+  left: 0;
+  color: rgba(206, 15, 0, 0.8);
+  font-size: 16px;
+  font-weight: 500;
+`;
+
+export { StyledInputBody, StyledInput, StyledValidate, StyledInputImg };
