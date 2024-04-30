5cfeb2ee141310ed973213d4ea79567e58e4e1a6
davidpinkevich <davidpinkevich95@gmail.com>
2024-04-30 17:37:55 +0300

    feat: add styles for random arts

---
+++ src/components/RandomArts/styled.ts
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+const StyledRandomArts = styled.div`
+  width: 100%;
+  margin-top: 40px;
+  font-family: "Lexend Deca";
+`;
+
+const StyledRandomTitle = styled.div`
+  text-align: center;
+  font-size: 16px;
+  line-height: 20px;
+  color: rgba(224, 164, 73, 1);
+`;
+
+const StyledRandomInfo = styled.div`
+  text-align: center;
+  font-size: 32px;
+  line-height: 40px;
+  margin-bottom: 40px;
+  color: rgba(57, 57, 57, 1);
+`;
+
+const StyledRandomItems = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  gap: 20px;
+`;
+
+export {
+  StyledRandomArts,
+  StyledRandomTitle,
+  StyledRandomInfo,
+  StyledRandomItems
+};
