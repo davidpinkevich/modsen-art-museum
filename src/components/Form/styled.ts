81f75116db796232288dbb6dc78581f438cd6fcb
davidpinkevich <davidpinkevich95@gmail.com>
2024-04-30 17:32:53 +0300

    feat: add styles for form

---
+++ src/components/Form/styled.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+const StyledFormBody = styled.div`
+  max-width: 762px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 72px;
+`;
+
+const StyledForm = styled.form`
+  width: 100%;
+`;
+
+const StyledFormTitle = styled.h2`
+  font-family: "Lexend Deca";
+  font-weight: 700;
+  font-size: 64px;
+  line-height: 80px;
+  text-align: center;
+  margin-top: 120px;
+  color: rgba(57, 57, 57, 1);
+  & span {
+    color: rgba(241, 121, 0, 1);
+  }
+`;
+
+export { StyledFormBody, StyledForm, StyledFormTitle };
