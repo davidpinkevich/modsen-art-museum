f499ba3ed6b0c1d5c6bd038132dcb5b6c2462bbb
davidpinkevich <davidpinkevich95@gmail.com>
2024-05-02 20:16:44 +0300

    feat: add items for main layout

--- src/pages/MainLayout/index.tsx
+++ src/pages/MainLayout/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 import { Header } from "@components/Header";
+import { Footer } from "@components/Footer";
 
 const MainLayout: React.FC = () => {
   return (
-    <div>
+    <>
       <Header />
-      <p>Let's find some art here!</p>
-    </div>
+      <Outlet />
+      <Footer />
+    </>
   );
 };
 
