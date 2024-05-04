3e109b633753d7ce03a54ada1c2a899e1fb8685f
davidpinkevich <davidpinkevich95@gmail.com>
2024-05-05 00:23:41 +0300

    feat: add test for filters

---
+++ src/components/Forms/Filters/Filters.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from "@testing-library/react";
+import { Filters } from ".";
+
+describe("Filters component", () => {
+  const handlerFilter = jest.fn();
+  const handlerPage = jest.fn();
+  test("render current active filter", () => {
+    render(
+      <Filters
+        filter="public"
+        load={false}
+        setFilter={handlerFilter}
+        setPage={handlerPage}
+      />
+    );
+
+    const mainFilter = screen.getByText("Public arts");
+    expect(mainFilter).toBeInTheDocument();
+    expect(mainFilter).toHaveAttribute("type", "active");
+  });
+
+  test("test other filters", () => {
+    render(
+      <Filters
+        filter="public"
+        load={false}
+        setFilter={handlerFilter}
+        setPage={handlerPage}
+      />
+    );
+
+    const nextFilter = screen.getByText("Non-public works");
+    expect(nextFilter).not.toHaveAttribute("type", "active");
+  });
+});
