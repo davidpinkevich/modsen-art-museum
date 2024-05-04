6c032ae4405e89ef5a05d982099e279e00173233
davidpinkevich <davidpinkevich95@gmail.com>
2024-05-05 00:39:38 +0300

    fix: change view card for adaptive

--- src/components/Card/styled.ts
+++ src/components/Card/styled.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { BREAKPOINTS } from "@constants/data";
 
 const StyledCardWrapper = styled.div<{ type: string }>`
   width: 100%;
@@ -21,14 +22,26 @@ const StyledCard = styled.div<{ type: string }>`
     box-shadow: 0px 0px 3px 1px rgba(0, 0, 0, 0.5);
     transform: scale(1.05);
   }
-  &:hover {
-    transform: scale(1);
+  @media (max-width: ${BREAKPOINTS.sm}) {
+    &:hover {
+      transform: scale(1);
+    }
   }
 `;
 
 const StyledCardImg = styled.img`
   width: 100%;
   height: 387px;
+  transition: all 0.2s;
+  &:hover {
+    box-shadow: 0px 0px 3px 2px rgba(0, 0, 0, 0.5);
+    transform: scale(1.01);
+  }
+  @media (max-width: ${BREAKPOINTS.sm}) {
+    &:hover {
+      transform: scale(1);
+    }
+  }
 `;
 
 export { StyledCard, StyledCardWrapper, StyledCardImg };
