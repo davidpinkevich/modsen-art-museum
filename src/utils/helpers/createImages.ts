c9664363ff070822e8a07ab241a6e73d2e50dbc7
davidpinkevich <davidpinkevich95@gmail.com>
2024-05-11 11:57:09 +0300

    refactor: optimize function create image

--- src/utils/helpers/createImages.ts
+++ src/utils/helpers/createImages.ts
@@ -1,23 +1,17 @@
+import { QUERY_IMAGE } from "@constants/data";
 import { type TypeArt } from "@src/types";
 
 const createImage = (src: string | null) => {
-  return src
-    ? `${process.env.BASE_URL_IMAGES}${src}/full/400,/0/default.jpg`
-    : null;
+  return src ? `${process.env.BASE_URL_IMAGES}${src}${QUERY_IMAGE}` : null;
 };
 
 const createImages = (items: Array<TypeArt | undefined>) => {
   return items
-    .map((item) => {
-      if (item) {
-        return {
-          ...item,
-          image_id: createImage(item.image_id)
-        };
-      }
-      return null;
-    })
-    .filter((item) => item !== null) as TypeArt[];
+    .filter((item): item is TypeArt => item !== undefined)
+    .map((item) => ({
+      ...item,
+      image_id: createImage(item.image_id)
+    }));
 };
 
 export { createImage, createImages };
